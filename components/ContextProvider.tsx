a8dc6fd5522c2b53fc11d2fbcf707e8ee4e41c2a
Ahmed Qadri <aqadri@launchdarkly.com>
2025-07-29 22:14:41 -0700

    updated workshop

--- components/ContextProvider.tsx
+++ components/ContextProvider.tsx
@@ -30,7 +30,7 @@ const ContextProvider = ({ children }: { children: React.ReactNode }) => {
             const FetchedProvider = await asyncWithLDProvider({
                 clientSideID: process.env.NEXT_PUBLIC_LD_CLIENT_KEY || "",
                 reactOptions: {
-                    useCamelCaseFlagKeys: true,
+                    useCamelCaseFlagKeys: false,
                 },
                 options: {
                     application: {
